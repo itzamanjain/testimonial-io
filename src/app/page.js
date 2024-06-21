7e67dbe8f10fb05ecb76464b4b3564cb4f0b3bf8
itzamanjain <aman.044063@tmu.ac.in>
2024-06-21 22:35:13 +0530

    added middleware

--- src/app/page.js
+++ src/app/page.js
@@ -6,14 +6,25 @@ function Page() {
   const router = useRouter();
   return (
     <>
-    <h1 className="text-3xl text-white">Collect Your Testimonials</h1>
-    <button
+      <div className="flex flex-col md:flex-row justify-center px-4">
+        <h1 className="text-3xl md:text-6xl font-bold text-white text-center">
+          Collect Your
+        </h1>
+        <span className="text-3xl md:text-6xl font-bold bg-clip-text ml-0 md:ml-4 text-transparent bg-gradient-to-r from-gradient-start via-gradient-middle to-gradient-end text-center">
+          Testimonials
+        </span>
+      </div>
+      <div className="flex justify-center mt-5">
+        <button
           type="button"
           onClick={() => router.push('/getTestimonials')}
-          className="bg-blue-500 mt-5 cursor-pointer ml-5 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          className="bg-blue-500 cursor-pointer hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
         >
           Get Your Testimonials
         </button>
+      </div>
+      
+      
     </>
   )
 }
---
+++ src/middleware.ts
@@ -0,0 +1,40 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+ 
+// This function can be marked `async` if using `await` inside
+export function middleware(request: NextRequest) {
+    // Redirect to the home page if the user is not authenticated
+
+    const path = request.nextUrl.pathname
+
+    const isPublicPath = path === '/signin' || path === '/signup'
+
+    const token = request.cookies.get('token')?.value || ''
+
+    if (isPublicPath && token) {
+        return NextResponse.redirect(new URL('/', request.url))
+    }
+
+    if (!isPublicPath && !token) {
+        return NextResponse.redirect(new URL('/signin', request.url))
+    }
+
+
+
+
+
+
+  //return NextResponse.redirect(new URL('/home', request.url))
+
+}
+ 
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: [
+    '/',
+    '/signup',
+    '/signin',
+    '/profile',
+
+  ]
+}
