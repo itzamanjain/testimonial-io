0bb344aa5d0f128896441eb92e1d7941e68c6674
itzamanjain <aman.044063@tmu.ac.in>
2024-06-24 18:10:32 +0530

    added img

--- src/app/getTestimonials/page.js
+++ src/app/getTestimonials/page.js
@@ -1,6 +1,7 @@
 "use client"
 import React, { useState } from 'react';
 import axios from 'axios';
+import Link from 'next/link';
 
 const Page = () => {
   const [url, setUrl] = useState('');
@@ -16,14 +17,22 @@ const Page = () => {
   };
 
   return (
-    <div className="flex justify-center items-center h-screen">
+    <div className="flex flex-col justify-center items-center min-h-screen p-4">
+      <h1 className='text-yellow-500 text-2xl text-center mb-4'>
+        Copy the URL after generation and share it with your customers to take testimonials
+      </h1>
       <button
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-4"
         onClick={generateUrl}
       >
-        Ask for Testimonials
+        Generate URL to take Testimonials
       </button>
-      {url && <h1 className="text-xl text-white ml-4">{url}</h1>}
+      {url && <h1 className="text-xl text-white text-center mb-4 break-words">{url}</h1>}
+      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4">
+        <Link href="/profile">
+          Go To Profile Page 
+        </Link>
+      </button>
     </div>
   );
 };
--- src/components/TestimonialCard.jsx
+++ src/components/TestimonialCard.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import Image from 'next/image';
 
-function TestimonialCard({ customerName, customerCompany, customerReview }) {
+function TestimonialCard({ customerName, customerCompany,customerSocialId, customerReview, customerPosition, customerImage }) {
   return (
-    <div className="bg-gray-800 p-5 rounded-lg">
-      <div className="text-lg font-bold mb-2">{customerName}</div>
-      <div className="text-sm text-gray-400 mb-2">{customerCompany}</div>
-      <div className="text-sm">{customerReview}</div>
+    <div className="bg-white w-full p-6 max-w-sm mx-auto rounded-lg shadow-lg flex items-center space-x-4">
+      <div className="flex-shrink-0">
+        <Image className="object-cover rounded-full" src="/testimonial.png" width={70} height={70} alt={customerName} />
+      </div>
+      <div>
+        <div className="text-xl font-medium text-black">{customerName}</div>
+        <p className="text-gray-500">{customerPosition} at {customerCompany}</p>
+        <p className="text-gray-500">@{customerSocialId}</p>
+        <p className="mt-2 text-gray-600">{customerReview}</p>
+      </div>
     </div>
   );
 }
